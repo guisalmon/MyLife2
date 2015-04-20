50d1e4d77cbdde91b917ea3bd665db0a9ea312ae
Guillaume Salmon <guisalmon@gmail.com>
2015-04-20 13:06:55 +0200

    Outlets and actions added for the add view

---
+++ MyLife/AddViewController.swift
@@ -0,0 +1,62 @@
+//
+//  AddViewController.swift
+//  MyLife
+//
+//  Created by Guest User on 20/04/15.
+//  Copyright (c) 2015 Guest User. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var titleView: UITextView!
+    @IBOutlet weak var dateView: UITextView!
+    @IBOutlet weak var textView: UITextView!
+    var post: Post!
+    var media: [UIImage]!
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        post = Post()
+        media = [UIImage]()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onPublishClicked(sender: AnyObject) {
+        post.title = titleView.text
+        post.text = textView.text
+        post.date = NSDate();
+        post.media = media
+    }
+    
+    @IBAction func onPhotoClicked(sender: AnyObject) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onGalleryClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func onVoiceClicked(sender: AnyObject) {
+    }
+    
+    @IBAction func onVideoClicked(sender: AnyObject) {
+    }
+    
+}
